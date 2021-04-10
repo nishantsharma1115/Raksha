fe2777cabb7b6f9b1705816a4b56ef420e084208
Nishant <official.nishant1115@gmail.com>
2021-04-11 01:57:23 +0530

    WhatsApp Share and Call Feature Added

--- app/src/main/java/com/baldeagles/raksha/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/baldeagles/raksha/ui/fragments/HomeFragment.kt
@@ -1,7 +1,15 @@
 package com.baldeagles.raksha.ui.fragments
 
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -32,6 +40,13 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             setHasFixedSize(false)
         }
 
+        binding.callPoliceBtn.setOnClickListener {
+            makePhoneCall()
+        }
+
+        binding.whatsappBtn.setOnClickListener {
+            sendWhatsAppMessage()
+        }
 
         val simpleItemTouchCallback = object :
             ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
@@ -65,5 +80,48 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             safeHouseAdapter.submitList(it)
         }
 
+    }
+
+    private fun sendWhatsAppMessage() {
+        val whatsappIntent = Intent(Intent.ACTION_SEND)
+        whatsappIntent.type = "text/plain"
+        whatsappIntent.setPackage("com.whatsapp")
+        whatsappIntent.putExtra(Intent.EXTRA_TEXT, "Help me... I am in Danger")
+        try {
+            requireActivity().startActivity(whatsappIntent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(requireContext(), "Whatsapp is not installed :(", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    private fun makePhoneCall() {
+        val phoneNumber = "1091"
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CALL_PHONE),
+                1
+            )
+        } else {
+            val dial = "tel:$phoneNumber"
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse(dial)))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall()
+            }
+        }
     }
 }

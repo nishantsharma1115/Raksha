8859cd03f2e3e95076a1d499f19a775014e8d023
Nishant <official.nishant1115@gmail.com>
2021-04-10 15:10:24 +0530

    Add font

--- app/src/main/java/com/baldeagles/raksha/ui/OnBoardingScreen.kt
+++ app/src/main/java/com/baldeagles/raksha/ui/OnBoardingScreen.kt
@@ -2,6 +2,7 @@ package com.baldeagles.raksha.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.baldeagles.raksha.R
 
 class OnBoardingScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

5abac5280a1aca4409e662d8b6cf0b87e5042ab4
Nishant <official.nishant1115@gmail.com>
2021-04-10 12:34:29 +0530

    New branch testing

--- app/src/main/java/com/baldeagles/raksha/ui/MainActivity.kt
+++ app/src/main/java/com/baldeagles/raksha/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.baldeagles.raksha.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.baldeagles.raksha.R
 
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "New Branch", Toast.LENGTH_SHORT).show()
     }
 }
